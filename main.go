d637bbecfd3a6485f741db8ff2d103b6979d48b3
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-09 14:18:40 +0200

    Call os.Getwd and os.ReadDir

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	print("SCOOP!\n")
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	scoop(dir)
+}
+
+func scoop(dir string) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	for _, file := range files {
+		fmt.Println(file)
+	}
 }
